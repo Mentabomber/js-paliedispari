4aa1e05c7fa981b2a1c9b3091f67a72ef6932da3
Menta <simcictilen.gmail.com>
2023-05-08 16:15:13 +0200

    cambio metodo risoluzione problema e ciclo for

--- js/main.js
+++ js/main.js
@@ -2,36 +2,56 @@
 // Chiedere all’utente di inserire una parola
 // Creare una funzione per capire se la parola inserita è palindroma
 
-// richiesta all'utente di inserire parola
+// variabili globali 
 
-// controllo se la parola ha un numero di caratteri pari o dispari
 
-// if( word.length % 2 )
-//     risultato = "pari";
+// richiesta all'utente di inserire una parola
+
+let userWord = prompt("inserisci una parola");
+
+
+
+// if( userWord.length % 2 )
+
+//      risultato = "pari";
+        
+
+
+        for (let i = 0, index = userWord.length; i < (userWord.length / 2); i++, index--){
+
+            let letterOne = userWord.charAt(i);
+
+            let letterTwo = userWord.charAt(userWord.length-index);
+
+            console.log(letterOne);
+
+            console.log(letterTwo);
+        };
+
+        // prendo il secondo 50% di caratteri e li metto dentro ad un'altra lista
+
+        // controllo se i caratteri dentro la prima lista combacino con la seconda lista
+
+        // aggiorno output
+   
+
+// SE la parola ha un numero di caratteri dispari rimuovo la lettera centrale e poi faccio il controllo che farei se la parola avesse un numero di caratteri pari 
+        
 // else
-//     risultato = "dispari";
 
-// se la parola ha un numero di caratteri pari controllo che il primo 50% di lettere dalla prima all'ultima sia uguale al secondo 50% di lettere dall'ultima alla prima 
+//      risultato = "dispari";
 
-    // prendo il primo 50% di caratteri e li metto dentro ad una lista
+        // divido il numero di caratteri della parola per 2 e arrotondo per eccesso il risultato e rimuovo il carattere in quella posizione 
 
-    // prendo il secondo 50% di caratteri e li metto dentro ad un'altra lista
+        // prendo il primo 50% di caratteri e li metto dentro ad una lista
 
-    // controllo se i caratteri dentro la prima lista combacino con la seconda lista
+        // prendo il secondo 50% di caratteri e li metto dentro ad un'altra lista
 
-    // aggiorno output
+        // controllo se i caratteri dentro la prima lista combacino con la seconda lista
 
-// se la parola ha un numero di caratteri dispari rimuovo la lettera centrale e poi faccio il controllo che farei se la parola avesse un numero di caratteri pari 
+        // aggiorno output   
 
-    // divido il numero di caratteri della parola per 2 e arrotondo per eccesso il risultato e rimuovo il carattere in quella posizione 
 
-    // prendo il primo 50% di caratteri e li metto dentro ad una lista
-
-    // prendo il secondo 50% di caratteri e li metto dentro ad un'altra lista
-
-    // controllo se i caratteri dentro la prima lista combacino con la seconda lista
-
-    // aggiorno output
 
 
 // output 
