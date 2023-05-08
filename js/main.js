c10b2a07dd9c6bb5d3f60528fc703f7673eae8c2
Menta <simcictilen.gmail.com>
2023-05-08 15:25:52 +0200

    commenti js

---
+++ js/main.js
@@ -0,0 +1,41 @@
+// Palindroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+// richiesta all'utente di inserire parola
+
+// controllo se la parola ha un numero di caratteri pari o dispari
+
+// if( word.length % 2 )
+//     risultato = "pari";
+// else
+//     risultato = "dispari";
+
+// se la parola ha un numero di caratteri pari controllo che il primo 50% di lettere dalla prima all'ultima sia uguale al secondo 50% di lettere dall'ultima alla prima 
+
+    // prendo il primo 50% di caratteri e li metto dentro ad una lista
+
+    // prendo il secondo 50% di caratteri e li metto dentro ad un'altra lista
+
+    // controllo se i caratteri dentro la prima lista combacino con la seconda lista
+
+    // aggiorno output
+
+// se la parola ha un numero di caratteri dispari rimuovo la lettera centrale e poi faccio il controllo che farei se la parola avesse un numero di caratteri pari 
+
+    // divido il numero di caratteri della parola per 2 e arrotondo per eccesso il risultato e rimuovo il carattere in quella posizione 
+
+    // prendo il primo 50% di caratteri e li metto dentro ad una lista
+
+    // prendo il secondo 50% di caratteri e li metto dentro ad un'altra lista
+
+    // controllo se i caratteri dentro la prima lista combacino con la seconda lista
+
+    // aggiorno output
+
+
+// output 
+
+// 1037
+
+// 518 1 518
